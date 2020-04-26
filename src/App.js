05639fa43db01bf0b0833b1b76acc84815c1125c
Whale shark <zinbeizame1206@gmail.com>
2020-04-26 20:44:47 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input
+        type="text"
+        onChange={() => {
+          console.log("I am clicked");
+        }}
+      />
+    </React.Fragment>
   );
 }
 
